8a7c3a59cf21f7ad4ed427df06f011b3dbea1635
Kostya <kyudytskiy@gmail.com>
2025-04-22 20:19:41 +0300

    Adds initial Python bindings for bsxplorer2
    Configures the crate as a Python extension module using `pyo3`.
    Introduces Python bindings for `ContextData` and related enums (`ReportTypeSchema`, `Strand`, `Context`).
    Exposes methods to convert `ContextData` to Polars DataFrames.
    Updates build configuration for `maturin`.

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,17 +1,8 @@
-pub fn add(
-    left: u64,
-    right: u64,
-) -> u64 {
-    left + right
-}
+use pyo3::prelude::*;
+mod data_structs;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[pymodule]
+fn bsx2(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    Ok(())
 }
