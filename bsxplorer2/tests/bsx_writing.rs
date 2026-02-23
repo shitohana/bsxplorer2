95a84e9f1c3847d21a10a18e891a34ca7ed7b3e0
Dnec <78984255+Michael14567@users.noreply.github.com>
2026-02-23 20:29:20 +0300

    restore test

---
+++ bsxplorer2/tests/bsx_writing.rs
@@ -0,0 +1,59 @@
+mod common;
+use std::fs::File;
+use std::path::PathBuf;
+
+use bsxplorer2::data_structs::typedef::BsxSmallStr;
+use bsxplorer2::io::bsx::{
+    BatchIndex,
+    BsxFileReader,
+    BsxFileWriter,
+};
+use itertools::Itertools;
+use rstest::{
+    fixture,
+    rstest,
+};
+use tempfile::NamedTempFile;
+
+#[fixture]
+fn test_bsxreader() -> BsxFileReader {
+    let reader = BsxFileReader::try_new(
+        File::open(
+            PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/data/report.bsx"),
+        )
+        .expect("Error opening test report file"),
+    )
+    .expect("Failed to create reader");
+    reader
+}
+
+#[rstest]
+fn write_bsx(mut test_bsxreader: BsxFileReader) -> anyhow::Result<()> {
+    let index = BatchIndex::from_reader(&mut test_bsxreader)?;
+    let sink_tempfile = NamedTempFile::new()?;
+    let mut writer = BsxFileWriter::try_new(
+        sink_tempfile.reopen()?,
+        &index
+            .get_chr_order()
+            .into_iter()
+            .map(BsxSmallStr::to_string)
+            .collect_vec(),
+        None,
+    )?;
+
+    for batch in test_bsxreader.iter() {
+        writer.write_batch(batch?)?;
+    }
+    writer.close()?;
+
+    let mut written_reader = BsxFileReader::try_new(sink_tempfile.reopen()?)?;
+
+    for (orig_batch, read_batch) in test_bsxreader.iter().zip(written_reader.iter()) {
+        let orig_batch = orig_batch.unwrap();
+        let read_batch = read_batch.unwrap();
+
+        assert_eq!(orig_batch, read_batch)
+    }
+
+    Ok(())
+}
